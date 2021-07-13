c3eab3894f313ccee899c4eef27d73b500736d0b
Levister Deltafun <amka.feremina@yandex.ru>
2021-07-13 23:23:53 +0300

    Animation v.0.6

--- MMORPG/View/MainWindow.xaml.cs
+++ MMORPG/View/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,21 +21,38 @@ namespace View
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Drawing.Size sizeWindows;
+        DoubleAnimation animation = new DoubleAnimation();
         public MainWindow()
         {
+            sizeWindows = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
             InitializeComponent();
         }
 
         private void SignInButton_Click(object sender, RoutedEventArgs e)
-        {
+        { 
             signInGrid.Visibility = Visibility.Hidden;
             MainWindowGrid.Visibility = Visibility.Visible;
+
+            animation.To = (sizeWindows.Width - 1200) / 2;
+            animation.AutoReverse = false;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(3));
+            BeginAnimation(LeftProperty, animation);
         }
 
         private void DoubleAnimation_Completed(object sender, EventArgs e)
         {
-            button1.Visibility = Visibility.Visible;
-            
+        }
+
+        private void WindowApp_Loaded(object sender, RoutedEventArgs e)
+        {
+            Left = (sizeWindows.Width - signInGrid.Width)/2;
+            Top = (sizeWindows.Height - signInGrid.Height)/2;
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
